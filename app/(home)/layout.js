26e064d5983293985ca4c2c720a4fae4513db9f7
TomDelavega <altommasson@yahoo.fr>
2024-07-18 13:25:16 +0200

    Hotfix on fonts

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -1,7 +1,45 @@
-import "../style/globals.scss";
-import { Inter } from "next/font/google";
+import {
+  Gochi_Hand,
+  Mynerve,
+  Reenie_Beanie,
+  Sedgwick_Ave_Display,
+  Handlee,
+} from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const font1 = Gochi_Hand({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-roboto-mono",
+  display: "swap",
+});
+
+const font2 = Mynerve({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font2",
+  display: "swap",
+});
+
+const font3 = Reenie_Beanie({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font3",
+  display: "swap",
+});
+
+const font4 = Sedgwick_Ave_Display({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font4",
+  display: "swap",
+});
+
+const fontText = Handlee({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--fontText",
+  display: "swap",
+});
 
 export const metadata = {
   title: "Fil Etnic",
@@ -10,8 +48,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      className={`${font1.variable} ${font2.variable} ${font3.variable} ${font4.variable} ${fontText.variable}`}
+    >
+      <body>{children}</body>
     </html>
   );
 }
